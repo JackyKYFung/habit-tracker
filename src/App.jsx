37e3d01243f16d0370e4688c0ce0f35d509e0dc1
Jacky Fung <jackyfung@Jackys-MacBook-Pro.local>
2026-03-13 17:50:52 -0700

    initial framework setupof app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ function App() {
 
   return (
     <>
-      
+      <main>
+        <h1>Habit Tracker</h1>
+        <div>
+          <input type="text" placeholder="enter new habit"/>
+          <button>Submit</button>
+        </div>
+
+        <div>
+          <ul>
+            habit name
+            progress bar
+          </ul>
+        </div>
+      </main>
     </>
   )
 }
