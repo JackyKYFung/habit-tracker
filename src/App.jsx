7dc3718c94bf284cd9c61b600c44df4b370a5103
Jacky Fung <jackyfung@Jackys-MacBook-Pro.local>
2026-03-16 12:21:26 -0700

    added toggle function for each habit + added habit adding function

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,23 @@ function App() {
     {id: 2, name: "Drink Mocha", completed: true}
   ]);
 
+  const [newItemName, setNewItemName] = useState("");
+
+  const addHabit = (e) => {
+    e.preventDefault(); // <-- prevent form from automatically reloading
+    
+    if (newItemName.trim() === "") return;
+
+    const newHabit = {
+      id: Date.now(),
+      name: newItemName,
+      completed: false
+    };
+
+    setHabitList([...habitList, newHabit]);
+    setNewItemName("");
+    console.log(habitList);
+  }
 
   const toggleHabit = id => {
     setHabitList(prevHabitList =>
@@ -21,10 +38,15 @@ function App() {
     <>
       <main>
         <h1>Habit Tracker</h1>
-        <div>
-          <input type="text" placeholder="enter new habit"/>
-          <button>Submit</button>
-        </div>
+        <form onSubmit={addHabit}>
+          <input 
+            type="text" 
+            value={newItemName} 
+            onChange={(e) => setNewItemName(e.target.value)} 
+            placeholder="enter new habit"/>
+
+          <button type="submit">Submit</button>
+        </form>
 
         <div>
           <ul>
@@ -32,9 +54,11 @@ function App() {
             {habitList.map((habit) => (
               <li
                 key={habit.id}
+                onClick={() => toggleHabit(habit.id)}
                 style={{
                   textDecoration: habit.completed ? "line-through" : "none",
-                  color: habit.completed ? "#999" : "#000"
+                  color: habit.completed ? "#999" : "#000",
+                  cursor: "pointer"
                 }}
               >
               <span>
